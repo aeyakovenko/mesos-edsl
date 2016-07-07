5cd957ddd5238bd0a8e8eb88f634be91dbf6daab
Anatoly Yakovenko <ayakovenko@mesosphere.com>
2016-07-07 14:18:03 -0700

    sbt file

--- build.sbt
+++ build.sbt
@@ -1,9 +1,11 @@
 lazy val root = (project in file(".")).
   settings(
-    name := "mesos-combinators",
+    name := "mesos-edsl",
     version := "1.0",
     scalaVersion := "2.11.7"
   )
 
 libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.0" % "test"
+unmanagedBase <<= baseDirectory { base => base / "mesos-0.28.2" }
+
 coverageEnabled := true
--- src/main/scala/MesosEdsl.scala
+++ src/main/scala/MesosEdsl.scala
@@ -2,19 +2,26 @@ package mesosedsl
 
 import cats.{Monad}
 //import org.apache.mesos.*;
-import org.apache.mesos.Protos.{TaskStatus};
+import org.apache.mesos.Protos.{TaskStatus}
 
-abstract class Scheduler[T:<Task] extends Monad[Scheduler[T]] {
-  case class Single(task:T, rs: List[Resource]) extends Scheduler
-  case class Sequence(ls:List[Scheduler]) extends Scheduler
-  case class Parallel(ls:List[Scheduler]) extends Scheduler
-  case class Resources(task:Scheduler, resources:List[Resources]) extends Scheduler
+class Scheduler[T <: T] extends Monad[Scheduler[T]] {
+  case class Single(task:T, rs: List[Resource[T]]) extends Scheduler[T]
+  case class Sequence(ls:List[Scheduler[T]]) extends Scheduler[T]
+  case class Parallel(ls:List[Scheduler[T]]) extends Scheduler[T]
+  case class Resources(task:Scheduler[T], resources:List[Resource[T]]) extends Scheduler[T]
   def run():Try[Seq[TaskStatus]]
 }
-abstract class Resource[T] extends Monad[Resource[T]] {
-  case class Cpu(cpu: Double) extends Resource
-  case class Memory(mem: Double) extends Resource
-  case class When(seconds: Int) extends Resource
-  case class Many(ls:List[ResourceT]) extends Resource
+
+class Resource[T :< T] extends Monad[Resource[T]] {
+  case class Cpu(cpu: Double) extends Resource[T]
+  case class Memory(mem: Double) extends Resource[T]
+  case class When(seconds: Int) extends Resource[T]
+  case class Many(ls:List[Resource[T]]) extends Resource[T]
   def with(sched:Schedule[T]):Schedule[T]
 }
+
+abstract class Task {
+  def execute():Unit
+}
+
+
