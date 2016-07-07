728a2307c2d378bd1d968acfa86c2d1d09245e27
Anatoly Yakovenko <ayakovenko@mesosphere.com>
2016-07-07 15:15:33 -0700

    builds

--- build.sbt
+++ build.sbt
@@ -2,10 +2,11 @@ lazy val root = (project in file(".")).
   settings(
     name := "mesos-edsl",
     version := "1.0",
-    scalaVersion := "2.11.7"
+    scalaVersion := "2.11.8"
   )
 
 libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.0" % "test"
-unmanagedBase <<= baseDirectory { base => base / "mesos-0.28.2" }
+libraryDependencies += "org.typelevel" %% "cats" % "0.6.0"
+libraryDependencies += "org.apache.mesos" % "mesos" % "0.28.1"
 
 coverageEnabled := true
--- src/main/scala/MesosEdsl.scala
+++ src/main/scala/MesosEdsl.scala
@@ -3,25 +3,44 @@ package mesosedsl
 import cats.{Monad}
 //import org.apache.mesos.*;
 import org.apache.mesos.Protos.{TaskStatus}
+import scala.util.{Try}
 
-class Scheduler[T <: T] extends Monad[Scheduler[T]] {
-  case class Single(task:T, rs: List[Resource[T]]) extends Scheduler[T]
-  case class Sequence(ls:List[Scheduler[T]]) extends Scheduler[T]
-  case class Parallel(ls:List[Scheduler[T]]) extends Scheduler[T]
-  case class Resources(task:Scheduler[T], resources:List[Resource[T]]) extends Scheduler[T]
-  def run():Try[Seq[TaskStatus]]
+class Scheduler[T]                                                extends Monad[Scheduler] {
+  case class Single(task:T)                                       extends Scheduler[T]
+  case class Sequence(tls:List[Scheduler[T]])                     extends Scheduler[T]
+  case class Parallel(tls:List[Scheduler[T]])                     extends Scheduler[T]
+  case class Resources(task:Scheduler[T], rls:List[Resource[T]])  extends Scheduler[T]
+
+  def schedule(): Try[Seq[TaskStatus]] = error("unimplemented")
+  def pure[A](x: A): Scheduler[A] = error("unimplemented")
+  def flatMap[A, B](fa: Scheduler[A])(f: A => Scheduler[B]): Scheduler[B] = error("unimplemented")
 }
 
-class Resource[T :< T] extends Monad[Resource[T]] {
-  case class Cpu(cpu: Double) extends Resource[T]
-  case class Memory(mem: Double) extends Resource[T]
-  case class When(seconds: Int) extends Resource[T]
-  case class Many(ls:List[Resource[T]]) extends Resource[T]
-  def with(sched:Schedule[T]):Schedule[T]
+class Resource[T]                         extends Monad[Resource] {
+  case class Cpu(cpu: Double)             extends Resource[T]
+  case class Memory(mem: Double)          extends Resource[T]
+  case class When(secs: Int)              extends Resource[T]
+  case class Many(rls:List[Resource[T]])  extends Resource[T]
+
+  def withResource(sched: Scheduler[T]): Scheduler[T] = error("unimplemented")
+
+  def pure[A](x: A): Resource[A] = error("unimplemented")
+  def flatMap[A, B](fa: Resource[A])(f: A => Resource[B]): Resource[B] = error("unimplemented")
+
 }
 
-abstract class Task {
-  def execute():Unit
+class Task {
+  def execute(): Unit = error("unimplemented")
 }
+
+//object Scheduler {
+//	//parallel builder, launch the tasks in parallel 
+//	def <||> (a: Scheduler[T], b: Scheduler[T]): Scheduler[T]
+//  def task(task:T): Scheduler[T]
+//
+//	//sequential builder
+//	def flatMap(builder:Scheduler[Scheduler[T]]):Scheduler[T]
+//
+//}
 
 
